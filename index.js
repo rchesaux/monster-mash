586094af2b5c430c90d15d1cffe93dd275d48b4a
rchesaux <chesaux66@gmail.com>
2024-10-23 10:28:42 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log('button clicked');
+
+fuction Monster (name, frogToes, newtEyes)
+    this.name = name;
+    this.frogToes = frogToes;
+    this.newtEyes = newtEyes;
+
+
+    Monster Monster1(String name, int frogToes, int newtEyes, String broomstickType) {
+        name = "Betty";
+        frogToes = 4;
+        newtEyes = 6;
+    }
+        
+        
+    
+}
